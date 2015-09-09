2e17f8bd23bb710dcec482e14775ab9c20b85ca4
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-09-09 09:50:39 -0700

    completed Navbar setup

--- public/app/actions/NavbarActions.js
+++ public/app/actions/NavbarActions.js
@@ -1,1 +1,42 @@
-NavbarActions.js
+import alt from '../alt';
+import {assign} from 'underscore';
+
+class NavbarActions {
+  constructor() {
+    this.generateActions(
+      'updateOnlineUsers',
+      'updateAjaxAnimation',
+      'updateSearchQuery',
+      'getQuestionCountSuccess',
+      'getQuestionCountFail',
+      'findQuestionSuccess',
+      'findQuestionFail'
+    );
+  }
+
+  findQuestion(payload) {
+    $.ajax({
+      url: '/api/questions/search',
+      data: { name: payload.searchQuery }
+    })
+      .done((data) => {
+        assign(payload, data);
+        this.actions.findQuestionSuccess(payload);
+      })
+      .fail(() => {
+        this.actions.findQuestionFail(payload);
+      });
+  }
+
+  getQuestionCount() {
+    $.ajax({ url: '/api/questions/count' })
+      .done((data) => {
+        this.actions.getQuestionCountSuccess(data)
+      })
+      .fail((jqXhr) => {
+        this.actions.getQuestionCountFail(jqXhr)
+      });
+  }
+}
+
+export default alt.createActions(NavbarActions);
