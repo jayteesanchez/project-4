8f94eb999757e1693f91e6649c530f0f3af9c680
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-09-10 22:29:18 -0700

    created server side routes and controller and model update for question

---
+++ server/config/express.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var MongoStore =  require('connect-mongo')(session);
+var path = require('path');
+var secrets = require('./secrets');
+var flash = require('express-flash');
+var methodOverride = require('method-override');
+
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+
+module.exports = function (app, passport) {
+  app.set('port', (process.env.PORT || 3000));
+
+  app.disable('x-powered-by');
+  app.set('views', path.join(__dirname, '..', 'views'));
+
+  app.set('view cache', false);
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+  app.use(methodOverride());
+  app.use(express.static(path.join(__dirname, '../..', 'public')));
+
+  app.enable('trust proxy');
+
+  app.use(cookieParser());
+
+  var sess = {
+    resave: true,
+    saveUninitialized: true,
+    // Use generic cookie name for security purposes
+    key: 'sessionId',
+    secret: secrets.sessionSecret,
+    // Add HTTPOnly, Secure attributes on Session Cookie
+    // If secure is set, and you access your site over HTTP, the cookie will not be set
+    cookie: {
+      httpOnly: true
+    },
+    store: new MongoStore({ url: secrets.db, autoReconnect: true})
+  };
+
+  var node_env = process.env.NODE_ENV;
+  console.log('Environment: ' + node_env);
+  if(node_env === 'production') {
+    sess.cookie.secure = true; // Serve secure cookies
+  }
+
+  app.use(session(sess));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(flash());
+
+  var port = (node_env === 'production') ? app.get('port') : 3000;
+
+  // We only run this workflow when not in Production && require a hot-loader
+  if(node_env === 'devhotloader') {
+    // We require the bundle inside the if block because
+    // it is only needed in a development environment.
+    var devServer = require('../dev-server');
+    devServer();
+
+    // Any request to localhost:3000 is proxied to webpack-dev-server
+    app.all('/assets/*', function(req, res) {
+      proxy.web(req, res, {
+          target: 'http://localhost:3001'
+      });
+    });
+
+  }
+
+  proxy.on('error', function(e) {
+    console.log('Could not connect to proxy, please try again...');
+  });
+
+};
--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -1,143 +1,88 @@
-var Question = require('../models/Question');
-
-// GET
-function getAll(request, response) {
-  Question.find(function(error, questions) {
-    if(error) response.json({message: 'Could not find any questions'});
-    response.json({questions: questions});
-  });
-}
-
-// POST
-function createQuestion(request, response) {
-  console.log('in Question');
-  console.log('body:',request.body);
-
-  var question = new Question({
-  question:             req.body.question,
-  choice1:              req.body.choice1,
-  choice1_img:          req.body.choice1_img,
-  choice2:              req.body.choice2,
-  choice2_img:          req.body.choice2_img,
-  expiration:           req.body.expiration,
-  user_id:              req.user.id
-});
-
-Question.prototype.expireAt = function() {
-  return (this.posted_date + this.expiration)
-}
-
-Question.prototype.totalVote = function() {
-  return (votes_choice_1 + votes_choice_2);
-}
-
-  question.save(function(error) {
-    if(error) response.json({messsage: 'Could not ceate question b/c:' + error});
-
-    response.json({question: question});
-  });
-}
-
-// GET
-function getOneQuestion(request, response) {
-  var id = request.params.id;
-
-  Question.findById({_id: id}, function(error, question) {
-    if(error) response.json({message: 'Could not find question b/c:' + error});
-
-    response.json({question: question});
-  });
-}
-
-function updateQuestion(request, response) {
-  var id = request.params.id;
-
-  Question.findById({_id: id}, function(error, question) {
-    if(error) response.json({message: 'Could not find question b/c:' + error});
-
-    if(request.body.question) question.question = request.body.question;
-    if(request.body.choice1) question.choice1 = request.body.choice1;
-    if(request.body.choice_1_img) question.choice_1_img = request.body.choice_1_img;
-    if(request.body.votes_choice_1) question.votes_choice_1 = request.body.votes_choice_1;
-    if(request.body.choice2) question.choice2 = request.body.choice2;
-    if(request.body.choice_2_img) question.choice_2_img = request.body.choice_2_img;
-    if(request.body.votes_choice_2) question.votes_choice_2 = request.body.votes_choice_2;
-    if(request.body.downVote) question.downVote = request.body.downVote;
-    if(request.body.expiration) question.expiration = request.body.expiration;
+var mongoose = require('mongoose');
+var _ = require('lodash');
+var Question = mongoose.model('Question');
 
 
-    Question.save(function(error) {
-      if(error) response.json({messsage: 'Could not update question b/c:' + error});
-
-      response.json({message: 'Question successfully updated', question: question});
-    });
-  });
-}
-
-function removeQuestion(request, response) {
-  var id = request.params.id;
-
-  Question.remove({_id: id}, function(error) {
-    if(error) response.json({message: 'Could not delete question b/c:' + error});
-
-    response.json({message: 'Question successfully deleted'});
-  });
-}
-
-function searchQuestions(request, response) {
-  var question = new RegExp(request.query.question, 'i');
-
-  Question.findOne({ question: question }, function(err, question) {
-    if (err) return next(err);
-
-    if (!question) {
-      return response.status(404).send({ message: 'Question not found.' });
+/**
+ * List
+ */
+function getAllQuestions(req, res) {
+  Question.find({}).exec(function(err, questions) {
+    if(!err) {
+      res.json(questions);
+    }else {
+      console.log('Error in first query');
     }
-
-    return response.send(question);
   });
 };
 
-function questionCount (request, response) {
-  Question.count({}, function(err, count) {
-    if (err) return next(err);
-    return response.send({ count: count });
+/**
+ * Add a Question
+ */
+function addQuestion(req, res) {
+  Question.create(req.body, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+    res.status(200).send('Added successfully');
   });
 };
 
-function topQuestions (request, response) {
-  var params = request.query;
-  var conditions = {};
+/**
+ * Update a Question
+ */
+function updateQuestion(req, res) {
+  var query = { id: req.body.id };
+  var isIncrement = req.body.isIncrement;
+  var isFull = req.body.isFull;
+  var omitKeys = ['id', '_id', '_v', 'isIncrement', 'isFull'];
+  var data = _.omit(req.body, omitKeys);
 
-  _.each(params, function(value, key) {
-    conditions[key] = new RegExp('^' + value + '$', 'i');
-  });
-
-  Question
-    .find(conditions)
-    .sort('totalVote')
-    .limit(100)
-    .exec(function(err, questions) {
-      if (err) return next(err);
-
-      questions.sort(function(a, b) {
-        if (a < b) { return 1; }
-        if (a > b) { return -1; }
-        return 0;
-      });
-
-      return response.send(questions);
+  if(isFull) {
+    Question.findOneAndUpdate(query, data, function(err, data) {
+      if(err) {
+        console.log('Error on save!');
+        res.status(500).send('We failed to save to due some reason');
+      }
+      res.status(200).send('Updated successfully');
     });
+  } else {
+    Question.findOneAndUpdate(query, { $inc: { count: isIncrement ? 1: -1 } }, function(err, data) {
+      if(err) {
+        console.log('Error on save!');
+        // Not sure if server status is the correct status to return
+        res.status(500).send('We failed to save to due some reason');
+      }
+      res.status(200).send('Updated successfully');
+    });
+  }
+
 };
 
+/**
+ *
+ */
+function increment(req, res) {
+  var query = { id: req.body.id };
+
+};
+
+/**
+ * Remove a Question
+ */
+function removeQuestion(req, res) {
+  var query = { id: req.body.id };
+  Question.findOneAndRemove(query, function(err, data) {
+    if(err) console.log('Error on delete');
+    res.status(200).send('Removed Successfully');
+  });
+};
 
 module.exports = {
-  getAll: getAll,
-  createQuestion: createQuestion,
-  getOneQuestion: getOneQuestion,
-  updateQuestion: updateQuestion,
-  removeQuestion: removeQuestion,
-  searchQuestions: searchQuestions,
-  questionCount: questionCount,
-  topQuestions: topQuestions
+  getAllQuestions: getAllQuestions,
+  addQuestion:     addQuestion,
+  updateQuestion:  updateQuestion,
+  increment:       increment,
+  removeQuestion:  removeQuestion
 }
